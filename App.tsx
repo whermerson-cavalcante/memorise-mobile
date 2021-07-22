f0a759239df9301dda0bc7122315dd3c365d0a42
whermerson-cavalcante <whermersonc19@hotmail.com>
2021-07-22 09:39:31 -0300

    first-commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Register } from './src/pages/Register';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Register />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/global/theme.ts
@@ -0,0 +1,18 @@
+export default {
+  colors: {
+    background: '#121212',
+    gray_background: '#242424',
+
+    primary: '#BB86FC',
+
+    success: '#00FF00',
+    attention: '#ff0000',
+
+    text: '#fff',
+
+    icons: '#484848'
+  },
+  fonts: {
+    
+  }
+}
